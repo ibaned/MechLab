83a89520fb3da3da285f10fa03041156623ce8cc
Brian Granzow <brian.granzow@gmail.com>
2017-05-23 11:41:49 -0400

    neumann: begin neumann evaluator registration

--- src/ml_neumann.cpp
+++ src/ml_neumann.cpp
@@ -1,8 +1,39 @@
+#include <goal_discretization.hpp>
+#include <goal_ev_basis.hpp>
+
 #include "ml_mechanics.hpp"
+
+using Teuchos::rcp;
+using goal::Traits;
 
 template <typename EvalT>
 void ml::Mechanics::register_neumann(goal::FieldManager fm) {
-  (void)fm;
+
+  std::vector<goal::Field*> disp = u;
+
+  //  get the current entity type to operate on
+  auto type = disc->get_side_type(side_set);
+
+  // bail if there are no entities to operate on in this side set
+  if (type < 0) {
+    goal::set_extended_data_type_dims(indexer, fm, 0);
+    fm->postRegistrationSetupForType<EvalT>(NULL);
+    return;
+  }
+
+  // set the displacement field basis functions
+  if (is_primal || is_dual) {
+    auto ev = rcp(new goal::Basis<EvalT, Traits>(disp[0], type));
+    fm->registerEvaluator<EvalT>(ev);
+  }
+
+  // compute tractions if needed for this side set
+  if (traction_map.count(side_set)) {
+  }
+
+  // set the FAD data and finalize the PHX field maanger registration.
+  goal::set_extended_data_type_dims(indexer, fm, type);
+  fm->postRegistrationSetupForType<EvalT>(NULL);
 }
 
 template void ml::Mechanics::register_neumann<goal::Traits::Residual>(goal::FieldManager fm);
